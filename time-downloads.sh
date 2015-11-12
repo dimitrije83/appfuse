2ec3d8e9c4ac49f4389131903f063162b6cebf05
Jason Swank <jason@scalene.net>
2015-11-12 19:15:20 +0000

    just download

---
+++ time-downloads.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+function init() {
+  mkdir -p ~/.m2
+}
+
+
+function reset_repo() {
+  rm -rf ~/.m2/repository || true
+}
+
+function use_google() {
+  cp google_settings.xml ~/.m2/settings.xml
+}
+
+function use_central() {
+  rm -rf ~/.m2/settings.xml || true
+}
+
+
+function main() {
+  time mvn dependency:resolve
+  reset_repo
+  use_google
+  time mvn dependency:resolve
+}
+
+main $*
