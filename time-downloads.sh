df17638ea6fdef19f97755c36a32b34215686a44
Jason Swank <jason@scalene.net>
2015-11-12 20:02:52 +0000

    correctly alternate

--- time-downloads.sh
+++ time-downloads.sh
@@ -10,39 +10,39 @@ function init() {
 function reset_repo() {
   mvn dependency:purge-local-repository -DactTransitively=false -DreResolve=false 1>/dev/null
   if [ -d ~/.m2/repository ]; then
-    du -skc ~/.m2/repository
+    rm -rf ~/.m2/repository
   fi
 }
 
 function use_google() {
   cp google_settings.xml ~/.m2/settings.xml
+  echo "\ngoogle mirror"
 }
 
 function use_central() {
   rm -rf ~/.m2/settings.xml || true
+  echo "\ndefault maven central"
 }
 
 
 function main() {
 
   reset_repo
-  echo "default maven central"
+  use_central
   time mvn dependency:resolve 1>/dev/null
   echo "---"
 
   reset_repo
-  echo "google mirror"
   use_google
   time mvn dependency:resolve 1>/dev/null
   echo "---"
 
   reset_repo
-  echo "default maven central"
+  use_central
   time mvn dependency:resolve 1>/dev/null
   echo "---"
 
   reset_repo
-  echo "google mirror"
   use_google
   time mvn dependency:resolve 1>/dev/null
   echo "---"
